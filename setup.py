c3d8f2d3802d3845160a87266e4d22b678f3742d
Cameron Lamb <jafacakes2011@hotmail.co.uk>
2018-03-25 19:32:11 +0100

    0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='MonzoOAuth',
-    version='0.1',
+    version='0.2.0',
     description='An OAuth2 Monzo Client',
     url='https://github.com/jafacakes2011/MonzoOAuth',
     author='Cameron',
@@ -11,6 +11,6 @@ setup(
     packages=['MonzoOAuth'],
     install_requires=[
       'oauth2client',
-  ],
+    ],
     zip_safe=False
 )
